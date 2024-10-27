dc69d63982d66ab8ce1e3b0ab9ac5d9ed844a3fc
Diego <creacioneszaide424@gmail.com>
2024-10-27 14:19:55 -0500

    ingresando las condicionasles

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ let numerosecreto = 7;
 //creo la variable adivinanza donde el usuario ingresa el numero
 let adivinanza = parseInt(prompt("ingrese el numero (entre 1 y 10)"));
 
+//comparo el numero con el numero secreo con las condicionales
+if(adivinanza === numerosecreto){
+    alert("correcto! adivinaste el número,");
+}else  if (adivinanza > numerosecreto){
+    alert("demaciado alto, intentalo con un numero menor,");
+
+}else {
+    alert("demaciado bajo, intentalo con un numero menor,");
+
+}
