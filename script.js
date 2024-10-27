f4e974106f24069ac341732f1067a835c3174e9c
Diego <creacioneszaide424@gmail.com>
2024-10-27 14:14:31 -0500

    creo la variable de adivinanza

---
+++ script.js
@@ -0,0 +1,6 @@
+// creo la variable donde va a estar asignado el numero a adivinar
+let numerosecreto = 7;
+
+//creo la variable adivinanza donde el usuario ingresa el numero
+let adivinanza = parseInt(prompt("ingrese el numero (entre 1 y 10)"));
+
